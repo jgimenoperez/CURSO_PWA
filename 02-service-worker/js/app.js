e0f8f39e776244d4730ce0e55ff88b3043f766fe
jgimenoperez <jgimenoperez@gmail.com>
2021-12-02 10:31:29 +0100

    cambios

---
+++ 02-service-worker/js/app.js
@@ -0,0 +1,14 @@
+
+// if ( 'serviceWorker' in navigator ) {
+//     console.log('Podemos usarlo!');
+// }
+
+// confirmar si podemos usar el SW
+if ( navigator.serviceWorker ) {
+
+    navigator.serviceWorker.register('/sw.js');
+    
+
+
+}
+
