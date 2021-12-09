1d43c39164f1951f419b80f8e47cf0e1bd43d8e0
jgimenoperez <jgimenoperez@gmail.com>
2021-12-09 22:37:37 +0100

    cambios

--- sw.js
+++ sw.js
@@ -41,15 +41,15 @@ const APP_SHELL_INMUTABLE = [
 self.addEventListener('install', e => {
 
 
-    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
-        cache.addAll( APP_SHELL ));
+    // const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+    //     cache.addAll( APP_SHELL ));
 
-    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
-        cache.addAll( APP_SHELL_INMUTABLE ));
+    // const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+    //     cache.addAll( APP_SHELL_INMUTABLE ));
 
 
 
-    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+    // e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
 
 });
 
