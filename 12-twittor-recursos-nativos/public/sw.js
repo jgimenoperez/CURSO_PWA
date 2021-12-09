b318ca0dc6c959a8c23ad6105d769b47ba4f8ac0
jgimenoperez <jgimenoperez@gmail.com>
2021-12-09 10:51:52 +0100

    cambios

--- 12-twittor-recursos-nativos/public/sw.js
+++ 12-twittor-recursos-nativos/public/sw.js
@@ -30,7 +30,7 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    // 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
     'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css',
     'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
     'https://cdn.jsdelivr.net/npm/pouchdb@7.0.0/dist/pouchdb.min.js'
